d5626ef0fadf1d335e78b3498c86484f7238f27a
jeremykendall <jeremy@jeremykendall.net>
2017-09-07 08:48:49 -0500

    Update implementation to match book

--- src/main/java/net/jeremykendall/refactoring/videostore/Rental.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/Rental.java
@@ -39,12 +39,9 @@ public class Rental {
     }
 
     public int getFrequentRenterPoints() {
-        // add frequent renter points
-        int frequentRenterPoints = 1;
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                getDaysRented() > 1) frequentRenterPoints ++;
-
-        return frequentRenterPoints;
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+            return 2;
+        else
+            return 1;
     }
 }
