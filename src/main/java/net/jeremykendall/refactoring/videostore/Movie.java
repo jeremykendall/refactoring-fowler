8264b86925da368c55638130eb0f3dcbe71a4a9b
jeremykendall <jeremy@jeremykendall.net>
2017-09-07 11:32:48 -0500

    Apply "Move Method" to Movie.getCharge

--- src/main/java/net/jeremykendall/refactoring/videostore/Movie.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/Movie.java
@@ -43,24 +43,7 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
