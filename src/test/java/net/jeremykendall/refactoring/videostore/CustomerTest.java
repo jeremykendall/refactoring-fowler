64126823c95b9cf9e2e0434d0ea5d97e4b71e466
jeremykendall <jeremy@jeremykendall.net>
2017-09-07 07:48:30 -0500

    Add test covering multiple rentals

--- src/test/java/net/jeremykendall/refactoring/videostore/CustomerTest.java
+++ src/test/java/net/jeremykendall/refactoring/videostore/CustomerTest.java
@@ -96,6 +96,22 @@ public class CustomerTest {
         assertEquals(expected, customer.statement());
     }
 
+    @Test
+    public void rentOneOfEachTypeForFourDays() throws Exception {
+        int daysRented = 4;
+        customer.addRental(getRental("Killer Klowns from Outer Space", Movie.REGULAR, daysRented));
+        customer.addRental(getRental("Awesome New Release", Movie.NEW_RELEASE, daysRented));
+        customer.addRental(getRental("Cars", Movie.CHILDRENS, daysRented));
+
+        String expected = "Rental Record for Arthur Dent\n" +
+                "\tKiller Klowns from Outer Space\t5.0\n" +
+                "\tAwesome New Release\t12.0\n" +
+                "\tCars\t3.0\n" +
+                "Amount owed is 20.0\n"+
+                "You earned 4 frequent renter points";
+        assertEquals(expected, customer.statement());
+    }
+
     private Rental getRental(String title, int priceCode, int daysRented) {
         Movie movie = new Movie(title, priceCode);
         return new Rental(movie, daysRented);
