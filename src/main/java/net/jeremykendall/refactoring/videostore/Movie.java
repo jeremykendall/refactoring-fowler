cd5cb491c55c8ebe7fcfbfe9944105b028d05e17
jeremykendall <jeremy@jeremykendall.net>
2017-09-07 10:35:37 -0500

    Use "Move Method" on getFrequentRenterPoints and getCharge

--- src/main/java/net/jeremykendall/refactoring/videostore/Movie.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/Movie.java
@@ -24,4 +24,32 @@ public class Movie {
     public String getTitle() {
         return _title;
     }
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
--- src/main/java/net/jeremykendall/refactoring/videostore/Rental.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/Rental.java
@@ -18,30 +18,10 @@ public class Rental {
     }
 
     public double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-
-        return result;
+        return _movie.getCharge(_daysRented);
     }
 
     public int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            return 2;
-        else
-            return 1;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
