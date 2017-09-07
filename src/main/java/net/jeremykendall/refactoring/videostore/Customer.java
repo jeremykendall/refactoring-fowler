1dbf2c29aa2f86741181692938c27dae75a0725e
jeremykendall <jeremy@jeremykendall.net>
2017-09-07 08:02:44 -0500

    Use Move Method to move charge calculation to Rental

--- src/main/java/net/jeremykendall/refactoring/videostore/Customer.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/Customer.java
@@ -21,7 +21,7 @@ public class Customer {
             Rental each = (Rental) rentals.nextElement();
 
             // determine amount for each line
-            thisAmount = amountFor(each);
+            thisAmount = each.getCharge();
 
             // add frequent renter points
             frequentRenterPoints ++;
@@ -38,27 +38,6 @@ public class Customer {
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) +
                 " frequent renter points";
-        return result;
-    }
-
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-
         return result;
     }
 
--- src/main/java/net/jeremykendall/refactoring/videostore/Rental.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/Rental.java
@@ -16,4 +16,25 @@ public class Rental {
     public int getDaysRented() {
         return _daysRented;
     }
+
+    public double getCharge() {
+        double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+
+        return result;
+    }
 }
