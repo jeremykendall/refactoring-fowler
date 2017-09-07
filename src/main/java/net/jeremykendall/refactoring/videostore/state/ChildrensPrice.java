c07e4bd0ebcf4b7eb5013fa3d9d61dd40f1faadd
jeremykendall <jeremy@jeremykendall.net>
2017-09-07 12:46:28 -0500

    Add braces around conditionals

--- src/main/java/net/jeremykendall/refactoring/videostore/state/ChildrensPrice.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/state/ChildrensPrice.java
@@ -11,8 +11,10 @@ public class ChildrensPrice extends Price {
     @Override
     public double getCharge(int daysRented) {
         double result = 1.5;
-        if (daysRented > 3)
+
+        if (daysRented > 3) {
             result += (daysRented - 3) * 1.5;
+        }
 
         return result;
     }
