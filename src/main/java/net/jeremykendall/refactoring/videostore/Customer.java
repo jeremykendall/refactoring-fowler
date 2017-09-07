652a0dcdc19f3e14243e5b93e76e399bb45502d5
jeremykendall <jeremy@jeremykendall.net>
2017-09-07 07:54:13 -0500

    Rename variables

--- src/main/java/net/jeremykendall/refactoring/videostore/Customer.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/Customer.java
@@ -41,25 +41,25 @@ public class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+    private double amountFor(Rental aRental) {
+        double result = 0;
+        switch (aRental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                result += 2;
+                if (aRental.getDaysRented() > 2)
+                    result += (aRental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                result += aRental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                result += 1.5;
+                if (aRental.getDaysRented() > 3)
+                    result += (aRental.getDaysRented() - 3) * 1.5;
                 break;
         }
-        
-        return thisAmount;
+
+        return result;
     }
 
     public void addRental(Rental arg) {
